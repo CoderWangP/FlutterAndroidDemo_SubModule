3c310cadb7e8438760b60304990bb6b8e6c7f990
王鹏 <wp674512015@163.com>
2021-09-25 11:31:54 +0800

    添加flutter子模块

--- app/src/main/java/com/example/flutterandroiddemo_submodule/MainActivity.kt
+++ app/src/main/java/com/example/flutterandroiddemo_submodule/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.flutterandroiddemo_submodule
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+
+    fun forward2Flutter(view: View) {
+        /*startActivity(FlutterActivity
+            .createDefaultIntent(this))*/
+        startActivity(
+            FlutterActivity
+                .withNewEngine()
+                .initialRoute("/page_2")
+                .build(this))
     }
 }
